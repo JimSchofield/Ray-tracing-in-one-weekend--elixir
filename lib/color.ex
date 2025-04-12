cf087559513aaffe9a65d530a02b4431ce474d9f
JimSchofield <23246869+JimSchofield@users.noreply.github.com>
2025-04-12 10:22:04 -0500

    Added gamma correction

--- lib/color.ex
+++ lib/color.ex
@@ -3,10 +3,19 @@ defmodule Color do
     V.new(r, g, b)
   end
 
+  def linear_to_gamma(num) do
+    if num > 0 do
+      :math.sqrt(num)
+    else
+      0
+    end
+  end
+
   def write_color(vec) do
     intensity = Interval.new(0.0, 0.999)
 
     color = vec
+      |> V.map(fn x -> linear_to_gamma(x) end)
       |> Interval.clamp(intensity)
       |> V.k(255.0)
       |> V.map(fn x -> trunc(x) end)
